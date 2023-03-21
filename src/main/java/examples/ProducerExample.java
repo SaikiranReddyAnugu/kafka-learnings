f9c4c19e5c7f94e776983a2d05b41238fa0e5f88
zadmin <saikiran.anugu@zemosolabs.com>
2023-03-21 15:21:56 +0530

    multiple producers and consumers with 2 brokers

---
+++ src/main/java/examples/ProducerExample.java
@@ -0,0 +1,103 @@
+package examples;
+
+import org.apache.kafka.clients.producer.*;
+
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class ProducerExample {
+
+    public static void main(final String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Please provide the configuration file path as a command line argument");
+            System.exit(1);
+        }
+
+        // Load producer configuration settings from a local file
+        final Properties props = loadConfig(args[0]);
+        Producer1(props);
+        Producer2(props);
+//        Producer3(props);
+//        Producer4(props);
+//        Producer5(props);
+
+    }
+
+
+    public static void Producer1(Properties props){
+        final String topic = "test1";
+
+        String[] users = {"eabara1", "jsmith1", "sgarcia1", "jbernard1", "htanaka1", "awalther1"};
+        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
+        ProduceData(props, users, items, topic);
+    }
+
+    public static void Producer2(Properties props){
+        final String topic = "test2";
+
+        String[] users = {"eabara2", "jsmith2", "sgarcia2", "jbernard2", "htanaka2", "awalther2"};
+        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
+        ProduceData(props, users, items, topic);
+    }
+
+    public static void Producer3(Properties props){
+        final String topic = "test3";
+
+        String[] users = {"eabara3", "jsmith3", "sgarcia3", "jbernard3", "htanaka3", "awalther3"};
+        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
+        ProduceData(props, users, items, topic);
+    }
+
+    public static void Producer4(Properties props){
+        final String topic = "test4";
+
+        String[] users = {"eabara4", "jsmith4", "sgarcia4", "jbernard4", "htanaka4", "awalther4"};
+        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
+        ProduceData(props, users, items, topic);
+    }
+
+    public static void Producer5(Properties props){
+        final String topic = "purchases";
+
+        String[] users = {"eabara5", "jsmith5", "sgarcia5", "jbernard5", "htanaka5", "awalther5"};
+        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
+        ProduceData(props, users, items, topic);
+    }
+
+
+
+    public static void ProduceData(Properties props,String[] users,String[] items, String topic){
+        try (final Producer<String, String> producer = new KafkaProducer<>(props)) {
+            final Random rnd = new Random();
+            final Long numMessages = 100L;
+            for (Long i = 0L; i < numMessages; i++) {
+                String user = users[rnd.nextInt(users.length)];
+                String item = items[rnd.nextInt(items.length)];
+
+                producer.send(
+                        new ProducerRecord<>(topic, user, item),
+                        (event, ex) -> {
+                            if (ex != null)
+                                ex.printStackTrace();
+                            else
+                                System.out.printf("Produced event to topic %s: key = %-10s value = %s%n", topic, user, item);
+                        });
+            }
+            System.out.printf("%s events were produced to topic %s%n", numMessages, topic);
+        }
+    }
+
+    // We'll reuse this function to load properties from the Consumer as well
+    public static Properties loadConfig(final String configFile) throws IOException {
+        if (!Files.exists(Paths.get(configFile))) {
+            throw new IOException(configFile + " not found.");
+        }
+        final Properties cfg = new Properties();
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            cfg.load(inputStream);
+        }
+        return cfg;
+    }
+}
